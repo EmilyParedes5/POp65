5bd76ff58c914e42814336e6331b222f9be25d51
EmilyParedes5 <eparedesm5@est.ups.edu.ec>
2024-09-20 12:44:12 -0500

    Commit Inicial

--- main.cpp
+++ main.cpp
@@ -1,47 +1,47 @@
 #include <iostream>
 using namespace std;
-void notadeberes(double n1){
+void notadeberes(double &n1){
     do{
         cout<<"Ingrese nota en deberes: ";
         cin>>n1;
-        if(n1>10 or n1<1){
+        if(n1>10 || n1<1){
             cout<<"Error limite de 10 puntos\n";
         }
-    }while(n1>10 or n1<1);
+    }while(n1>10 || n1<1);
 }
 
-void notatrabajos(double n2){
+void notatrabajos(double &n2){
     do{
         cout<<"Ingrese nota en trabajos: ";
         cin>>n2;
-        if(n2>10 or n2<1){
+        if(n2>10 || n2<1){
             cout<<"Error limite de 10 puntos\n";
         }
-    }while(n2>10 or n2<1);
+    }while(n2>10 || n2<1);
 
 }
 
-void notapruebas(double n3){
+void notapruebas(double &n3){
     do{
         cout<<"Ingrese nota en pruebas: ";
         cin>>n3;
-        if(n3>10 or n3<1){
+        if(n3>10 || n3<1){
             cout<<"Error limite de 10 puntos\n";
         }
-    }while(n3>10 or n3<1);
+    }while(n3>10 || n3<1);
 
 
 }
 
-void notaexamenes(double n4){
+void notaexamenes(double &n4){
 
     do{
         cout<<"Ingrese nota en Examen: ";
         cin>>n4;
-        if(n4>10 or n4<1){
+        if(n4>10 || n4<1){
             cout<<"Error limite de 10 puntos\n";S
         }
-    }while(n4>10 or n4<1);
+    }while(n4>10 || n4<1);
 }
 
 int main (){
